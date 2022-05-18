90e204cf7f5743cfa92493168a2da6a0595b173e
Ibtasam-67 <90761234+Ibtasam-67@users.noreply.github.com>
2022-05-18 06:28:47 -0700

    bugs in deployment

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
 import classes from "./MeetupDetail.module.css";
+import Image from "next/image";
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} />
+      <Image src={props.image} alt='' />
       <h1> {props.title}</h1>
       <address>{props.address}</address>
       <p> {props.description}</p>
